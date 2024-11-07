d2ce3d6a32237ead8c0a6b68c87cfa9dd64f7a15
Abdullah Al Mansur <almahfuz2019@gmail.com>
2024-11-07 16:26:34 +0600

    added urls

--- app.ts
+++ app.ts
@@ -20,7 +20,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://www.legalmatterbd.com/"],
+    origin: [
+      "https://www.legalmatterbd.com/",
+      "https://www.legalmatterbd.com",
+      "www.legalmatterbd.com",
+    ],
     credentials: true,
   }),
 );
